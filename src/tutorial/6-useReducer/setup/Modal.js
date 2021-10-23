f41b40a4ab8699bd6dbbd5e0023d79ae3e0a3960
tahir kalim <thrkalim@gmail.com>
2021-10-23 23:51:50 +0200

    useReducer practice

--- src/tutorial/6-useReducer/setup/Modal.js
+++ src/tutorial/6-useReducer/setup/Modal.js
@@ -1,4 +1,4 @@
-//reducer
+//1.reducer
 // import React, { useEffect } from 'react'
 
 // const Modal = () => {
@@ -7,12 +7,29 @@
 
 // export default Modal
 
+//2.reducer
+// import React, { useEffect } from 'react'
+
+// const Modal = ({modalContent}) => {
+//   return <div className='modal'>
+//     <p>{modalContent}</p>
+//       </div>
+
+// export default Modal
+
 import React, { useEffect } from 'react'
 
-const Modal = ({modalContent}) => {
-  return <div className='modal'>
-    <p>{modalContent}</p>
-      </div>
+const Modal = ({ modalContent, closeModal }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      closeModal()
+    }, 3000)
+  })
+  return (
+    <div className='modal'>
+      <p>{modalContent}</p>
+    </div>
+  )
+}
 
 export default Modal
-
--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -3,40 +3,30 @@ import { data } from '../../../data'
 
 // more components
 // fix - context api, redux (for more complex cases)
+
 const PropDrilling = () => {
   const [people, setPeople] = useState(data)
-  const removePerson = (id) => {
-    setPeople((people) => {
-      return people.filter((person) => person.id !== id)
-    })
-  }
+
   return (
     <section>
-      <h2>prpo drilling</h2>
-      <List people={people} removePerson={removePerson} />
+      <h3>prop drilling</h3>
+      <List people={people} />
     </section>
   )
 }
-const List = ({ people, removePerson }) => {
+const List = ({ people }) => {
   return (
     <>
       {people.map((person) => {
-        return (
-          <SinglePerson
-            key={person.id}
-            {...person}
-            removePerson={removePerson}
-          />
-        )
+        retrun(<SinglePerson key={person.id} />)
       })}
     </>
   )
 }
-const SinglePerson = ({ id, name, removePerson }) => {
+const SinglePerson = ({ id, name }) => {
   return (
     <div className='item'>
-      <h4>{name}</h4>
-      <button onClick={() => removePerson(id)}>Remove on click</button>
+      <h4>single item</h4>
     </div>
   )
 }
